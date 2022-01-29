020ec08895e9651cb9ee60e98a353f67e9076fc1
kaustubhn1 <narkhedekaustubh.think@gmail.com>
2022-01-30 00:36:10 +0530

    ui is added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, render_template
 from flask_mysqldb import MySQL
 from datetime import datetime
 
@@ -29,64 +29,77 @@ with app.app_context():
     #Closing the cursor
     cursor.close()
 
-@app.route("/first_name/<string:first_name>")
-def get_first_name(first_name):
-    f_name_is_lower = first_name.islower()
-    is_space_not_available = True if ' ' not in first_name else False
-    is_length_less_than_21_char = True if len(first_name) < 21 else False
+@app.route("/first_name", methods =["POST"])
+def get_first_name():
+    if request.method == "POST":
+        first_name = request.form.get("fname")
+        f_name_is_lower = first_name.islower()
+        is_space_not_available = True if ' ' not in first_name else False
+        is_length_less_than_21_char = True if len(first_name) < 21 else False
 
-    if f_name_is_lower and is_space_not_available and is_length_less_than_21_char:
+        if f_name_is_lower and is_space_not_available and is_length_less_than_21_char:
+            cursor = mysql.connection.cursor()
+            cursor.execute(''' INSERT INTO first_name_table VALUES('{0}')'''.format(first_name))
+            mysql.connection.commit()
+            cursor.close()
+            return render_template('response.html',route_name="first_name",value=first_name, valid=True)
+        else :
+            return render_template('response.html',route_name="first_name",value=first_name, valid=False)
+
+@app.route("/last_name", methods =["POST"])
+def get_last_name():
+    if request.method == "POST":
+        last_name = request.form.get("lname")
+        f_name_is_lower = last_name.islower()
+        is_space_not_available = True if ' ' not in last_name else False
+        is_length_less_than_21_char = True if len(last_name) < 21 else False
+
+        if f_name_is_lower and is_space_not_available and is_length_less_than_21_char:
+            cursor = mysql.connection.cursor()
+            cursor.execute(''' INSERT INTO last_name_table VALUES('{0}')'''.format(last_name))
+            mysql.connection.commit()
+            cursor.close()
+            return render_template('response.html',route_name="last_name",value=last_name, valid=True)
+        else :
+            return render_template('response.html',route_name="last_name",value=last_name, valid=False)
+
+
+@app.route("/cnic", methods =["POST"])
+def get_cnic():
+    if request.method == "POST":
+        cnic = request.form.get("cnic")
+        is_length_less_than_14_char = True if len(cnic) < 14 else False
+        clean_cnic_no = ''.join(char for char in cnic if char.isdigit())
+
+        if is_length_less_than_14_char :
+            cursor = mysql.connection.cursor()
+            cursor.execute(''' INSERT INTO cnic_table VALUES('{0}')'''.format(clean_cnic_no))
+            mysql.connection.commit()
+            cursor.close()
+            return render_template('response.html',route_name="Cnic NO",value=cnic, valid=True)
+        else :
+            return render_template('response.html',route_name="Cnic NO",value=cnic, valid=False)
+
+
+@app.route("/date", methods =["POST"])
+def get_date_of_birth():
+    if request.method == "POST":
+        input_date = request.form.get("dob")
+        try:
+            date_of_birth = datetime.strptime(input_date, '%Y-%m-%d').date()
+        except ValueError:
+            return render_template('response.html',route_name="Date OF Birth",value=input_date, valid=False)
         cursor = mysql.connection.cursor()
-        cursor.execute(''' INSERT INTO first_name_table VALUES('{0}')'''.format(first_name))
+        cursor.execute(''' INSERT INTO date_of_birth_table VALUES('{0}')'''.format(date_of_birth))
         mysql.connection.commit()
         cursor.close()
-        return f"your fist_Name has been saved in database : {first_name}</p>"
-    else :
-        return f"fist_name is not valid: {first_name}</p>"
+        return render_template('response.html',route_name="Date OF Birth",value=date_of_birth, valid=True)
 
-@app.route("/last_name/<string:last_name>")
-def get_last_name(last_name):
-    f_name_is_lower = last_name.islower()
-    is_space_not_available = True if ' ' not in last_name else False
-    is_length_less_than_21_char = True if len(last_name) < 21 else False
-
-    if f_name_is_lower and is_space_not_available and is_length_less_than_21_char:
-        cursor = mysql.connection.cursor()
-        cursor.execute(''' INSERT INTO last_name_table VALUES('{0}')'''.format(last_name))
-        mysql.connection.commit()
-        cursor.close()
-        return f"your last_Name has been saved in database : {last_name}</p>"
-    else :
-        return f"last_name is not valid: {last_name}</p>"
-
-@app.route("/cnic/<string:cnic>")
-def get_cnic(cnic):
-    is_length_less_than_14_char = True if len(cnic) < 14 else False
-    clean_cnic_no = ''.join(char for char in cnic if char.isdigit())
-
-    if is_length_less_than_14_char :
-        cursor = mysql.connection.cursor()
-        cursor.execute(''' INSERT INTO cnic_table VALUES('{0}')'''.format(clean_cnic_no))
-        mysql.connection.commit()
-        cursor.close()
-        return f"your cnic has been saved in database : {cnic}</p>"
-    else :
-        return f"your cnic no is not valid: {cnic}</p>"
-
-@app.route("/date/<string:date_of_birth>")
-def get_date_of_birth(date_of_birth):
-    try:
-        input_date = datetime.strptime(date_of_birth, '%Y-%m-%d').date()
-    except ValueError:
-        return f"<p>Invalid date : {date_of_birth}</p>"
-    cursor = mysql.connection.cursor()
-    cursor.execute(''' INSERT INTO date_of_birth_table VALUES('{0}')'''.format(date_of_birth))
-    mysql.connection.commit()
-    cursor.close()
-    return f"<p>saving date into db : {input_date}</p>"
     
-@app.route("/province/<string:province>")
-def get_province(province):
+@app.route("/province", methods =["POST"])
+def get_province():
+    if request.method == "POST":
+        province = request.form.get("province_input")
     valid_province_list = ["sindh", "punjab", "kpk", "gilgit baltistan"]
     is_valid_province = True if province.lower() in valid_province_list else False
 
@@ -95,11 +108,12 @@ def get_province(province):
         cursor.execute(''' INSERT INTO province_table VALUES('{0}')'''.format(province))
         mysql.connection.commit()
         cursor.close()
-        return f"your province has been saved in database : {province}</p>"
+        return render_template('response.html',route_name="province Name",value=province, valid=True)
     else :
-        return f"province is not valid: {province}</p>"
+        return render_template('response.html',route_name="province Name",value=province, valid=False)
+
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home_route():
+    return render_template('home.html')
 
